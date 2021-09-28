b45b2ce2cb74e619e4cb4ea59f20484924b998a2
Akeem C <javantieclarke@gmail.com>
2021-09-27 23:27:06 -0400

    added model for user

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+
+    emial: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please enter a valid email address",
+      ],
+      trim: true,
+    },
+
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thoughts",
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { 
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
